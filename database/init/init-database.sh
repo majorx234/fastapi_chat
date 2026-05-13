5b084f2f18e57908017f036e651ca0c8489ebc5d
MajorX234 <majorx234@googlemail.com>
2026-05-14 00:37:45 +0200

    Add docker build/init script for postgres database

---
+++ database/init/init-database.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_PASSWORD';
+    CREATE DATABASE $DATABASE_NAME;
+    GRANT ALL PRIVILEGES ON DATABASE $DATABASE_NAME TO $DATABASE_USER;
+    \c $DATABASE_NAME $POSTGRES_USER
+    GRANT ALL ON SCHEMA public TO $DATABASE_USER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$DATABASE_USER" --dbname "$DATABASE_NAME" <<-EOSQL
+    CREATE TABLE chatuser
+           (id SERIAL PRIMARY KEY,
+            username varchar(255),
+            email varchar(255) UNIQUE,
+            role varchar(20) NOT NULL UNIQUE,
+            hashed_password varchar(255) NOT NULL);
+    INSERT INTO chatuser (role)
+    VALUES
+      ('admin'),
+      ('chatter'),
+      ('ai'),
+      ('viewer');
+    INSERT INTO chatuser (username, email, role, hashed_password)
+           VALUES ('testuser1', 'test1@suer.de', 'chatter', 'ACDC');
+    INSERT INTO chatuser (username, email, role, hashed_password)
+           VALUES ('testuder2', 'test2@suer.de', 'chatter', '1337');
+EOSQL
+
