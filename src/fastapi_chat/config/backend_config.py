3250786b2cd565086cf6ad71306e7c59cb682cb0
MajorX234 <majorx234@googlemail.com>
2026-05-14 00:14:36 +0200

    Add database config

--- src/fastapi_chat/config/backend_config.py
+++ src/fastapi_chat/config/backend_config.py
@@ -4,6 +4,13 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
+    # webserver
     host: str = '0.0.0.0'
     port: int = 8000
     allowed_origins: Set[str] = set(["http://0.0.0.0:8000"])
+
+    # database
+    db_hostname: str = Field(default="db", alias="POSTGRES_HOST")
+    db_port: int = 5432
+    db_user:str = "testuser"
+    db_password: str = "1312"
